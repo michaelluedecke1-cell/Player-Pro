ba45e1593a9e4150fe9ad485632a305db67f1fac
michaelluedecke1-cell <michael.luedecke1@googlemail.com>
2026-05-18 20:19:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ const ASSETS = [
   'manifest.json'
 ];
 
-// 1. Dateien beim Installieren in den Cache laden
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,7 +12,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// 2. Alten Cache beim Aktivieren löschen
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -28,11 +26,9 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// 3. Ladeanfragen abfangen und aus dem Cache bedienen (Offline-Modus)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Wenn es im Cache ist, nutze es, andernfalls hole es aus dem Netzwerk
       return cachedResponse || fetch(event.request);
     })
   );
